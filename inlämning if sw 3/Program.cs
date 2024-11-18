a282bda3c0818c2adb7c899570e70714102f23de
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-11-18 12:57:41 +0100

    Add project files.

---
+++ inlämning if sw 3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Uppgift_3._10
+{
+    class Program
+
+
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Hur många timmar vill du hyra bilen? ");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int kostnad;
+
+            
+            if (timmar * 80 > 950)
+            {
+                kostnad = 950;
+            }
+            else
+            {
+                kostnad = timmar * 80;
+            }
+
+            {
+                Console.WriteLine("Kostnaden för att hyra bilen är: " + kostnad + " kr.");
+            }
+
+              
+        }
+    }
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+    
+
+
+
